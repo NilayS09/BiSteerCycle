0e7bd12f296646cde332dfede637f27f01d4e010
Nilay Srivastava <nilaynilanshu@gmail.com>
2023-06-05 08:13:33 +0530

    deleted not useful files

--- MATLAB/BiSteer2D/translate.m
+++
@@ -1,7 +0,0 @@
-function  s_shape = translate(shape,shift)
-% shape is a 2xn array with each row containing some x,y coordinate of the
-% array shape.
-% shift is 2x1 array specifying the amount by which the shape needs to move
-
-s_shape = shape+shift;
-end
